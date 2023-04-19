d51d40f817bcb9efcc5472b6ca49f194262017a1
sharon <moumanitokosharon@gmail.com>
2023-04-20 00:54:11 +0200

    add variable and movement

---
+++ FlyBall.java
@@ -0,0 +1,24 @@
+import processing.core.PApplet;
+
+public class Line extends PApplet{
+    public static void main(String[] args){
+        PApplet.runSketch(new String[]{"line"},new Line());
+    
+    }
+    public void settings(){
+        size(640,360);
+    }
+
+    public void setup(){
+        background(150,200,60);
+    } 
+    int x = 100;
+    int y =180;
+    public void draw(){
+        background(150,200,60);
+        fill(130,65,200);
+        stroke(200,235,100);
+        circle(x,y,150);
+         x=x+1;
+    }
+}
