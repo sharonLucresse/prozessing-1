35ef39bef5aaaf5b64d5d9c55acaf484fac4a68b
sharon <moumanitokosharon@gmail.com>
2023-04-19 03:11:22 +0200

    add ellipse

---
+++ Line.java
@@ -0,0 +1,17 @@
+import processing.core.PApplet;
+
+public class Line extends PApplet{
+    public static void main(String[] args){
+        PApplet.runSketch(new String[]{"line"},new Line());
+    
+    }
+    public void settings(){
+        size(600,600);
+    }
+    
+    public void draw(){
+      ellipse(150,200,50,50);
+      ellipse(250,200,50,50);
+     
+    }
+}
