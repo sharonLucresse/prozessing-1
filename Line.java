fd2282461db26c471b8f6462ac6e54d653c9db12
sharon <moumanitokosharon@gmail.com>
2023-04-19 23:16:41 +0200

    set background

--- Line.java
+++ Line.java
@@ -6,16 +6,18 @@ public class Line extends PApplet{
     
     }
     public void settings(){
-        size(600,600);
+        size(640,360);
+    }
+    public void setup(){
+        background(255,100,50);
     }
     
     public void draw(){
-      background(255,100,50);
-      ellipse(150,200,50,50);
+     ellipse(150,200,50,50);
       ellipse(250,200,50,50);
       triangle(200,230,180,240,220,240);
       stroke(250,0,0);
       fill(0,150,0);
-      rect(150,250,100,20);
+      rect(mouseX,mouseY,100,20);
     }
 }
