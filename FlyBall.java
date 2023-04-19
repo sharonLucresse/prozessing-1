623898cf8e057ae9d33bb464a7bf9030937ac62a
sharon <moumanitokosharon@gmail.com>
2023-04-20 01:42:35 +0200

    add speedY

--- FlyBall.java
+++ FlyBall.java
@@ -15,18 +15,21 @@ public class Line extends PApplet{
     int x = 100;
     int y =180;
     int speedX=1;
+    int speedY=1;
     public void draw(){
         background(150,200,60);
         fill(130,65,200);
         stroke(200,235,100);
         circle(x,y,150);
         x=x+speedX;
-        if(x==640){
-            speedX=-1;
+        y=y+speedY;
+        if(x==640 || x==0){
+            speedX=-speedX;
         }
-        if(x==0){
-            speedX=1;
+        if(y==360 || y==0){
+            speedY=-speedY;
         }
+        
         
         
             
